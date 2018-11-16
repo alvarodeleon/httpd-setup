992a032b9e0f2b5a6592c7a068627a207392f0bc
Unknown <deleon@adl.com.uy>
2018-11-16 20:20:50 -0300

    Create docker scripts

---
+++ docker-setup-basic.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p $2/{html,mysql,backup,disk,nginx}
+
+docker run -it --privileged --hostname="server" -e "container=docker" --cap-add SYS_ADMIN \
+--tmpfs /run -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+-p 22:22 -p 80:80 -p 3306:3306 \
+-v "$2/html:/var/www/html" \
+-v "$2/nginx:/etc/nginx/conf.d" \
+-v "$2/mysql/:/var/lib/mysql" \
+-v "$2/backup/:/backup" \
+-v "$2/disk/:/mnt/disk" \
+--name $1 "alvarodeleon/centos7-with-systemd" "/usr/sbin/init"
