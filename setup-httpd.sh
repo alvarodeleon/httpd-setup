1b8a47cf3343292a73c0d3989b0cb43892e49586
Unknown <deleon@adl.com.uy>
2019-03-13 17:49:30 -0300

    add ssh2

--- setup-httpd.sh
+++ setup-httpd.sh
@@ -27,8 +27,22 @@ php71-php-mysql php71-php-mcrypt php71-php-mbstring php71-php-cli php71-php-fpm 
 php72-php-mysql php72-php-mcrypt php72-php-mbstring php72-php-cli php72-php-fpm php72-php-gd php72-php-json php72-php-ioncube-loader php72-php-intl php72-php-pdo php72-php-pgsql php72-php-soap php72-php-xml php72-php-xmlrpc \
 php73-php-mysql php73-php-mcrypt php73-php-mbstring php73-php-cli php73-php-fpm php73-php-gd php73-php-json php73-php-ioncube-loader php73-php-intl php73-php-pdo php73-php-pgsql php73-php-soap php73-php-xml php73-php-xmlrpc \
 
+#Agregador sporte SSH2
+yum install -y libssh2-devel php54-php-pecl-ssh2 php55-php-pecl-ssh2 php56-php-pecl-ssh2 php70-php-pecl-ssh2 php71-php-pecl-ssh2 php72-php-pecl-ssh2 php73-php-pecl-ssh2
+
+
 yum -y install python2-pip python34 python34-pip python36 python36-pip
 yum -y install phpmyadmin
+
+
+#
+echo "extension=ssh2.so" >> /opt/remi/php54/root/etc/php.ini
+echo "extension=ssh2.so" >> /opt/remi/php55/root/etc/php.ini
+echo "extension=ssh2.so" >> /opt/remi/php56/root/etc/php.ini
+echo "extension=ssh2.so" >> /opt/remi/php56/root/etc/php.ini
+echo "extension=ssh2.so" >> /etc/opt/remi/php71/php.ini
+echo "extension=ssh2.so" >> /etc/opt/remi/php72/php.ini
+echo "extension=ssh2.so" >> /etc/opt/remi/php73/php.ini
  
 #Detenemos estos servicios
 systemctl stop php54-php-fpm
